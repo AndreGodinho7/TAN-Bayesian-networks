e905c4a17419ac62b38338520d58611356bd78d9
ivancarapinha <56005467+ivancarapinha@users.noreply.github.com>
2020-05-05 23:55:35 +0100

    MyTreeNode (classe) e TreeNode (interface) adicionados

---
+++ project/TreeNode.java
@@ -0,0 +1,8 @@
+package project;
+
+public interface TreeNode {
+    public void setNijkc(String[] children);
+    public void setNKijc(String[] children);
+    public void setTheta_ijkc();
+    public void setTheta_c();
+}
